4d94f56a09515d2fed62dde50620b8fa7af479ea
Assem Hossam <41378936+assemhossam99@users.noreply.github.com>
2022-03-27 02:15:17 +0200

    Add files via upload

--- cf_solutions.py
+++ cf_solutions.py
@@ -8,8 +8,9 @@ finished_ids = set()
 inf = 10000000
 done = False
 savedProblems = 0
-if not os.path.isdir('solutions'):
-    os.makedirs('solutions')
+wait_cnt = 0
+if not os.path.isdir(f'{username}\'s solutions'):
+    os.makedirs(f'{username}\'s solutions')
 for page in range(1, inf):
     print(f'page {page} starts...')
     print(' ')
@@ -35,18 +36,20 @@ for page in range(1, inf):
             problem_number = ''
             problem = str(submission.a['href'])
             problem_name = submission.text
+            if problem[1] == 'c':
+                contest_gym = 0
+            else:
+                contest_gym = 1
+                continue
             while not problem[-1].isdigit() or len(problem) > 20:
                 problem = problem[:-1]
             for c in problem:
                 if c >= '0' and c <= '9':
                     problem_number += c
-            if problem[1] == 'c':
-                contest_gym = 0
-            else:
-                contest_gym = 1
         elif idx % 8 == 4:
             language = submission.text
         elif idx % 8 == 5:
+            wait_cnt = 0
             status = submission.text
             if 'Accepted' in status and contest_gym == 0:
                 print(problem_name.strip())
@@ -59,9 +62,14 @@ for page in range(1, inf):
                         code = 'archive-name.zip'
                         continue
                     if 'archive-name.zip' in code:
+                        if wait_cnt == 2:
+                            print(f'could not save problem {problem_name}.')
+                            print('')
+                            break
                         print('please wait..')
                         time.sleep(300)
-                if code == None:
+                        wait_cnt += 1
+                if code == None or 'archive-name.zip' in code:
                     continue
                 print(submission_id.strip())
                 ex = 'txt'
@@ -79,7 +87,7 @@ for page in range(1, inf):
                 problem_name = problem_name[1:]
                 problem_name = letter + '-' + problem_name
                 print(problem_name)
-                with open(f'solutions/{problem_name.strip()}.{ex}', 'w') as f:
+                with open(f'{username}\'s solutions/{problem_name.strip()}.{ex}', 'w') as f:
                     f.write(code)
                 savedProblems = savedProblems + 1
                 print(f'problem [{problem_name.strip()}] saved successfully. {savedProblems} problems were saved successfully.')
